804da02973d34315cac82c73a8ef213881f8dbe7
ewgenTGM <ewgenbi@gmail.com>
2021-05-03 16:48:39 +0300

    Добавил функций в апишку passRecovery login logout me

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,7 +6,8 @@ const FROM = 'test-front-admin <test@test.com>';
 
 const instance = axios.create({baseURL: LOCAL_URL});
 
-export const Api = {
+export const api = {
+
   passRecovery(email: string) {
     const htmlBody = `<div style="background-color: lime; padding: 15px">
                        password recovery link: 
@@ -18,6 +19,37 @@ export const Api = {
       from: FROM,
       message: htmlBody
     };
-    return instance.post('/auth/forgot', reqBody);
+    return instance.post<InfoResponseType>('/auth/forgot', reqBody).then(res => res.data);
+  },
+
+  login(email: string, password: string, rememberMe: boolean = false) {
+    return instance.post<LoginResponseType>('/auth/login', {email, password, rememberMe}).then(res => res.data);
+  },
+
+  logout() {
+    return instance.delete<InfoResponseType>('/auth/me', {}).then(res => res.data);
+  },
+
+  me() {
+    return instance.post<LoginResponseType>('/auth/me', {}).then(res => res.data);
   }
 };
+
+type LoginResponseType = {
+  _id: string;
+  email: string;
+  name: string;
+  avatar?: string;
+  publicCardPacksCount: number; // количество колод
+  created: Date;
+  updated: Date;
+  isAdmin: boolean;
+  verified: boolean; // подтвердил ли почту
+  rememberMe: boolean;
+  error?: string;
+}
+
+type InfoResponseType = {
+  info: string
+  error: string
+}
